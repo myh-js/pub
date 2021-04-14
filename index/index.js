71fbd1c4a565df398665d0534b2217554d055d04
myh-js <myhjs@Foxmail.com>
2021-04-14 20:14:41 +0800

    3

---
+++ index/index.js
@@ -0,0 +1,13 @@
+﻿window.onload=function(){
+    symbol (s);
+    document.getElementById("ss").onclick =()=>
+{
+document.getElementById("ssnr").value="搜索内容";
+}
+    setInterval("document.getElementById('time').innerHTML='北京时间:'+new Date().toLocaleString();", 1000);
+}
+
+
+
+
+
