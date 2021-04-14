dffb6d285b568473ce7bb670d22980f938a17f68
myh-js <myhjs@Foxmail.com>
2021-04-14 23:15:27 +0800

    3

--- sign/sign.js
+++ sign/sign.js
@@ -1,1 +1,11 @@
-﻿
+﻿function emailcheck(){
+    var x=document.forms["dlform"]["zh"].value;
+    if(!/^\d+$/.test(x)){
+     alert("账号应为数字");
+    }
+    var y=document.forms["dlform"]["mm"].value;
+    if(!/^[a-zA-Z]{1,}/.test(y))
+    {
+        alert("密码首位应为字母")
+    }
+}
